b6f133d66813bb1d74caa81ccb746c8d33702645
Alexandr Chudopal <alex.ch@core-tech.ru>
2020-11-26 19:43:46 +0300

    added structure for network

--- __init__.py
+++ __init__.py
@@ -1,10 +1,42 @@
 from flask import (
     Flask,
-    jsonify
+    jsonify,
+    request,
 )
 
-app = Flask(__file__)
+from .generator_of_currency import get_currency_network
+
+APP = Flask(__name__)
 
 @app.route("/get_currency", methods=["POST", "GET"])
 def get_currency(request):
-    pass
+    """
+        {
+            "timeline": {
+                "begin": "xx-xx-xxxx",
+                "end": "xx-xx-xxxx",
+            },
+            "currency_name": [
+                "usd",
+                "eur",
+            ]
+        }
+    """
+    if request.method == "POST":
+        timeline_begin = request.data["timeline"]["begin"]
+        timeline_end = request.data["timeline"]["end"]
+        currency_name = request.data["currency"]
+
+        currencies = get_currency_network(
+            timeline_begin,
+            timeline_end,
+            currency_name
+        )
+
+        response_data = {
+            "currencies": currencies
+        }
+
+        return jsonify(response_data)
+
+    return "Hello world"
---
+++ generator_of_currency.py
@@ -0,0 +1,9 @@
+import time
+
+
+def get_currency_network(
+    timeline_begin, 
+    timeline_end, 
+    currency_name: str
+):
+    pass
