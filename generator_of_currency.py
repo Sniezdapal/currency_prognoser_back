72bd9ac434af5068b58c80d794c767171a8e872a
Alexandr Chudopal <alex.ch@core-tech.ru>
2020-12-07 01:09:55 +0300

    added requests to bank

--- generator_of_currency.py
+++ generator_of_currency.py
@@ -7,15 +7,17 @@ from flask import (
 from datetime import datetime
 import json
 import requests
+import urllib3
+
 
 APP = Flask(__name__)
 CURRENCY_NUMBERS = {
-    "usd": 145,
-    "eur": 19,
+    "USD": 145,
+    "EUR": 292,
 }
-BANK_URL = "https://www.nbrb.by/api/exrates/rates/{0}?ondate={1}"
+BANK_URL = "https://www.nbrb.by/api/exrates/rates/{0}"
 
-@APP.route("/get_currency", methods=["POST", "GET"])
+@APP.route("/get_currency", methods=["GET"])
 def get_currency():
     """
         {
@@ -27,38 +29,53 @@ def get_currency():
             ]
         }
     """
-    if request.method == "POST":
-        data = request.json
-        timeline_begin = datetime.fromtimestamp(int(data["begin"]))
-        timeline_end = datetime.fromtimestamp(int(data["end"]))
-        currency_names = data["currency_names"]
+    data = request.json
+    timeline_begin = datetime.fromtimestamp(int(data["begin"]))
+    timeline_end = datetime.fromtimestamp(int(data["end"]))
+    currency_names = data["currency_names"]
+    currencies = get_chart_data(
+        timeline_begin,
+        timeline_end,
+        currency_names,
+    )
+    print(currencies)
+    response_data = {
+        "currencies": currencies
+    }
+    return jsonify(currencies)
 
-        currencies = get_currency_network(
-            timeline_begin,
-            timeline_end,
-            currency_names
-        )
-        response_data = {
-            "currencies": currencies
-        }
 
-        print("HERE")
-
-        return jsonify(response_data)
-
-    return "Hello world"
-
-def buid_chart_data(begin_date, end_date):
+def get_chart_data(begin_date, end_date, currency_names):
+    currencies = {}
     if begin_date < datetime.now():
         for currency_name in currency_names:
+            print("HERE")
             currency_name = currency_name.upper()
-            date_string = date_begin.strftime("%Y-%m-%d")
-            currency_name = 145
-            url = BANK_URL.format(currency_numbers[currency_name], date_string)
-            currency = requests.get(url).text
-            currencies.append(currency)
+            date_string = begin_date.strftime("%Y-%m-%d")
+            url = BANK_URL.format(CURRENCY_NUMBERS[currency_name])
+
+            requests.packages.urllib3.disable_warnings()
+            requests.packages.urllib3.util.ssl_\
+                .DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+            try:
+                requests\
+                    .packages\
+                    .urllib3\
+                    .contrib\
+                    .pyopenssl\
+                    .util.ssl_\
+                    .DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+            except AttributeError:
+                pass
+
+            currency = requests.get(url, params={"ondate" : date_string}).text  
+            print(currency)
+            currencies[currency_name] = currency
+    return currencies
+    
 
 def make_request(date, currency_name):
-    
+    pass
+
 
 APP.run(host="0.0.0.0", port=5000, debug=True)
