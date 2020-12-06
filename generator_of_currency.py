b9535c0d3157cabe782ddb5ef54b35f82651c4ee
Alexandr Chudopal <alex.ch@core-tech.ru>
2020-12-07 01:50:39 +0300

    create period in query

--- generator_of_currency.py
+++ generator_of_currency.py
@@ -15,7 +15,7 @@ CURRENCY_NUMBERS = {
     "USD": 145,
     "EUR": 292,
 }
-BANK_URL = "https://www.nbrb.by/api/exrates/rates/{0}"
+BANK_URL = "https://www.nbrb.by/API/ExRates/Rates/Dynamics/{0}"
 
 @APP.route("/get_currency", methods=["GET"])
 def get_currency():
@@ -39,19 +39,23 @@ def get_currency():
         currency_names,
     )
     print(currencies)
-    response_data = {
-        "currencies": currencies
-    }
+
     return jsonify(currencies)
+
+
+def correct_date(begin_date, end_date):
+    dates = []
+    if begin_date < end_date:
+        pass
 
 
 def get_chart_data(begin_date, end_date, currency_names):
     currencies = {}
     if begin_date < datetime.now():
         for currency_name in currency_names:
-            print("HERE")
             currency_name = currency_name.upper()
-            date_string = begin_date.strftime("%Y-%m-%d")
+            begin = begin_date.strftime("%Y-%m-%d")
+            end = end_date.strftime("%Y-%m-%d")
             url = BANK_URL.format(CURRENCY_NUMBERS[currency_name])
 
             requests.packages.urllib3.disable_warnings()
@@ -68,8 +72,8 @@ def get_chart_data(begin_date, end_date, currency_names):
             except AttributeError:
                 pass
 
-            currency = requests.get(url, params={"ondate" : date_string}).text  
-            print(currency)
+            currency = requests\
+                .get(url, params={"startDate" : begin, "endDate": end }).text
             currencies[currency_name] = currency
     return currencies
     
